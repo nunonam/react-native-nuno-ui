636bc083db6724781761f525500f47c584008634
nunonam <nunonam@nunonamui-MacBookPro.local>
2020-07-03 02:09:23 +0900

    - text update

--- src/Text/index.js
+++ src/Text/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import {Text} from 'react-native';
 import { Nuno } from '../..';
 
-export default ({text, fontSize, fontWeight, color}) => {
+export default ({text, fontSize, fontWeight, color, style}) => {
   return (
     <Text
       style={{
         fontSize: fontSize,
         fontWeight: fontWeight,
         color: color || Nuno.config.textColor,
+        ...style
       }}>
       {text}
     </Text>
