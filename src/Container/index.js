1442d8b4213447f82a0eeeaecac09392eeb317a5
nunonam <nunonam@nunonamui-MacBookPro.local>
2020-07-08 16:41:35 +0900

    - update container

--- src/Container/index.js
+++ src/Container/index.js
@@ -4,24 +4,36 @@ import Text from '../Text';
 import Modal from '../Modal';
 import Seperator from '../Seperator';
 import Button from '../Button';
+import { Nuno, HView } from '../..';
 
-export default ({children, alertVisible, alertSetVisible, alertTitle, alertText, alertButtonColor}) => {
-  // const [visible, setVisible] = React.useState(false);
+export default ({children, alertVisible, alertTitle, alertText, alertButtonColor, onBackdropPress, onConfirm, onCancel}) => {
   return (
     <View style={{flex: 1}}>
       {children}
       <Modal
         isVisible={alertVisible}
-        onBackdropPress={() => alertSetVisible(false)}>
+        onBackdropPress={onBackdropPress}>
         <View
           style={{padding: 20, backgroundColor: 'white', borderRadius: 5}}>
           <View style={{flex: 1}} />
           <View>
-            <Text fontSize={22} fontWeight={'bold'} color={'black'} text={alertTitle} />
-            <Seperator height={50} />
-            <Text fontSize={16} fontWeight={'500'} color={'dimgray'} text={alertText} />
-            <Seperator height={50} />
-            <Button text={'확인'} color={alertButtonColor} onPress={() => alertSetVisible(false)} stretch />
+            <Text fontSize={18} fontWeight={'bold'} color={'black'} text={alertTitle} style={{textAlign: 'center'}} />
+            <Seperator height={30} />
+            <Text fontSize={16} fontWeight={'500'} color={'dimgray'} text={alertText} style={{textAlign: 'center'}} />
+            <Seperator height={30} />
+            <HView>
+              {onCancel && (
+                <>
+                  <View style={{flex: 1}}>
+                    <Button text={'확인'} color={alertButtonColor || 'gray'} onPress={onConfirm} stretch />
+                  </View>
+                  <Seperator width={10} />
+                </>
+              )}
+              <View style={{flex: 1}}>
+                <Button text={'확인'} color={alertButtonColor || Nuno.config.themeColor} onPress={onConfirm} stretch />
+              </View>
+            </HView>
           </View>
           <View style={{flex: 1}} />
         </View>
