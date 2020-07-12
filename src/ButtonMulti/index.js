a4cfeb900e3c54a03f6c0a949379a1d2857d543d
nunonam <nunonam@nunonamui-MacBookPro.local>
2020-07-12 13:32:25 +0900

    - update buttonMulti

--- src/ButtonMulti/index.js
+++ src/ButtonMulti/index.js
@@ -5,7 +5,7 @@ import HView from '../HView';
 import Text from '../Text';
 import { Nuno } from '../..';
 
-export default function ButtonMulti({items, selectedItems, text, color, stretch, textColor, borderColor, borderRadius}) {
+export default function ButtonMulti({items, selectedItems, onPress, color, textColor, borderColor, borderRadius}) {
   return (
     <HView style={{flexWrap: 'wrap'}}>
       {items.map((e) => {
@@ -17,12 +17,14 @@ export default function ButtonMulti({items, selectedItems, text, color, stretch,
               alignItems: 'center',
               paddingHorizontal: 20,
               paddingVertical: 5,
+              marginRight: 4,
+              marginBottom: 4,
               backgroundColor: selectedItems.filter(f => f.code == e.code).length > 0 ? Nuno.config.themeColor : color,
               borderRadius: borderRadius !== undefined ? borderRadius : 4,
               borderWidth: 1,
               borderColor: borderColor ? borderColor :  (color === 'white' ? 'darkgray' : color),
             }}
-            onPress={disable || loading ? null : onPress}>
+            onPress={onPress}>
             <Text
               fontSize={14}
               color={textColor ? textColor : (color === 'white' ? 'darkgray' : 'white')}
