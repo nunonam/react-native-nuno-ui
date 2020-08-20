9ae2b01d1a73d3367e7bc9c91014d0c5fa5b581b
nunonam <nunonam@nunonamui-MacBookPro.local>
2020-08-20 22:58:54 +0900

    - update chat

--- src/Chat/index.js
+++ src/Chat/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, FlatList} from 'react-native';
+import {View, FlatList, TouchableOpacity} from 'react-native';
 import Text from '../Text';
 import Modal from '../Modal';
 import Seperator from '../Seperator';
@@ -9,33 +9,68 @@ import TextInput from '../TextInput';
 import Image from '../Image';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import KeyboardSpacer from 'react-native-keyboard-spacer';
+import {getBottomSpace} from 'react-native-iphone-x-helper';
+import { Nuno } from 'react-native-nuno-ui';
+import moment from 'moment';
+import {screenWidth} from '../style';
 
-export default ({messages, me}) => {
-  const [visible, setVisible] = React.useState(true);
-  const renderItem = ({item}) => {
-    if (me.id === item.user.id) {
+export default ({messages, me, emptyAvatar, fontSize, leftComponent, onSend}) => {
+  const [message, setMessage] = React.useState('');
+
+  const renderItem = ({item, index}) => {
+    const currentTimestamp = new Date(item.createdAt).toLocaleString('ko-KR', { hour: 'numeric', minute: 'numeric', hour12: true });
+    const prevTimestamp = index > 0 && new Date(messages[index-1].createdAt).toLocaleString('ko-KR', { hour: 'numeric', minute: 'numeric', hour12: true });
+    const currentDate = moment(new Date(item.createdAt)).format('YYYY년 MM월 DD일');
+    const nextDate = index < messages.length - 1 && moment(new Date(messages[index+1].createdAt)).format('YYYY년 MM월 DD일');
+    if (me.id === item.id) {
       return (
-        <HView style={{flex: 1, alignItems: 'flex-start', justifyContent: 'flex-end', paddingHorizontal: 20, paddingVertical: 10}}>
-          <Seperator width={50} />
-          <View style={{flex: 1, alignItems: 'flex-end'}}>
-            <View style={{borderRadius: 5, backgroundColor: 'lightgray', padding: 10}}>
-              <Text fontSize={14} color={'dimgray'} text={item.text} />
+        <>
+          {currentDate !== nextDate && (
+            <View style={{padding: 20}}>
+              <Seperator line />
+              <View style={{position: 'absolute', top: 10, alignItems: 'center', width: screenWidth}}>
+                <View style={{backgroundColor: 'white', paddingHorizontal: 20}}>
+                  <Text fontSize={fontSize || 14} color={'gray'} text={currentDate} />
+                </View>
+              </View>
             </View>
-          </View>
-          {/* <Image uri={item.user.avatar} height={50} width={50} borderRadius={25} onPress={() => null} /> */}
-          <Seperator width={10} />
-          <FontAwesome name={'user-circle'} color={'lightgray'} size={40} />
-        </HView>
+          )}
+          <HView style={{flex: 1, alignItems: 'flex-start', justifyContent: 'flex-end', paddingHorizontal: 15, paddingVertical: 2}}>
+            <Seperator width={70} />
+            <View style={{flex: 1, alignItems: 'flex-end'}}>
+              <View style={{borderTopLeftRadius: 20, borderBottomLeftRadius: 20, borderTopRightRadius: 20, borderBottomRightRadius: 5, backgroundColor: Nuno.config.themeColor, padding: 10}}>
+                <Text fontSize={fontSize || 14} color={'white'} text={item.text} />
+                {currentTimestamp !== prevTimestamp && (
+                  <View style={{position: 'absolute', left: -60, bottom: 0}}>
+                    <Text fontSize={14} color={'darkgray'} text={currentTimestamp} />
+                  </View>
+                )}
+              </View>
+            </View>
+          </HView>
+        </>
       );
     } else {
       return (
-        <HView style={{flex: 1, alignItems: 'flex-start', justifyContent: 'flex-start', paddingHorizontal: 20, paddingVertical: 10}}>
-          {/* <Image uri={item.user.avatar} height={50} width={50} borderRadius={25} onPress={() => null} /> */}
-          <FontAwesome name={'user-circle'} color={'lightgray'} size={40} />
+        <HView style={{flex: 1, alignItems: 'flex-start', justifyContent: 'flex-start', paddingHorizontal: 15, paddingVertical: 2}}>
+          {((index < messages.length - 1 && item.id !== messages[index+1].id) || index+1 === messages.length) ? (
+            item.avatar ? (
+              <Image uri={item.avatar} height={36} width={36} borderRadius={18} onPress={() => null} />
+            ) : (
+              <Image local uri={emptyAvatar} height={36} width={36} borderRadius={18} onPress={() => null} />
+            )
+          ) : (
+            <View style={{width: 36}} />
+          )}
           <Seperator width={10} />
           <View style={{flex: 1, alignItems: 'flex-start'}}>
-            <View style={{borderRadius: 5, backgroundColor: 'lightgray', padding: 10}}>
-              <Text fontSize={14} color={'dimgray'} text={item.text} />
+            <View style={{borderTopLeftRadius: 20, borderBottomLeftRadius: 5, borderTopRightRadius: 20, borderBottomRightRadius: 20, backgroundColor: 'lightgray', paddingVertical: 10, paddingHorizontal: 15}}>
+              <Text fontSize={fontSize || 14} color={'black'} text={item.text} />
+              {currentTimestamp !== prevTimestamp && (
+                <View style={{position: 'absolute', right: -70, bottom: 0}}>
+                  <Text fontSize={14} color={'darkgray'} text={currentTimestamp} />
+                </View>
+              )}
             </View>
           </View>
           <Seperator width={50} />
@@ -43,11 +78,20 @@ export default ({messages, me}) => {
       );
     }
   };
+  const send = () => {
+    if (message) {
+      setMessage('');
+      onSend({
+        text: message,
+        avatar: me.avatar
+      });
+    }
+  };
   return (
     <View style={{flex: 1}}>
       <FlatList
         data={messages}
-        keyExtractor={item => item.id}
+        keyExtractor={item => item.index}
         renderItem={renderItem}
         inverted={true}
         // ListEmptyComponent={<Empty />}
@@ -59,15 +103,23 @@ export default ({messages, me}) => {
         // }}
       />
       <Seperator marginBottom={5} />
-      <HView style={{paddingHorizontal: 20}}>
-        <View style={{flex: 1}}>
-          <TextInput placeholder={'메세지를 입력해주세요'} onChangeText={() => null} value={''} />
+      <HView style={{paddingHorizontal: 20, borderTopWidth: 1, borderTopColor: 'lightgray', paddingVertical: 10}}>
+        {leftComponent}
+        <View style={{flex: 1, borderWidth: 1, borderColor: 'lightgray', borderRadius: 20}}>
+          <HView>
+            <View style={{flex: 1, paddingLeft: 13}}>
+              <TextInput placeholder={'메세지를 입력해주세요'} onChangeText={(e) => setMessage(e)} value={message} borderWidth={0} />
+            </View>
+            <TouchableOpacity onPress={() => send()} style={{paddingHorizontal: 20}}>
+              <Text text={'전송'} fontSize={14} fontWeight={'bold'} />
+            </TouchableOpacity>
+          </HView>
         </View>
-        <Seperator width={10} />
-        <Text text={'send'} />
       </HView>
       <Seperator bottom />
-      <KeyboardSpacer />
+      {Platform.OS === 'ios' && (
+        <KeyboardSpacer topSpacing={-getBottomSpace()} />
+      )}
     </View>
   );
 }
