7482f90ca75e0597128860bbaf1365eb4ddaa0ef
nunonam <nunonam@nunonamui-MacBookPro.local>
2020-08-28 11:59:57 +0900

    update func

--- funcs.js
+++ funcs.js
@@ -315,6 +315,11 @@ export function showToast(msg) {
     },
   });
 }
+export const swap = (arr, index1, index2) => arr.map((val, idx) => {
+  if (idx === index1) return arr[index2];
+  if (idx === index2) return arr[index1];
+  return val;
+})
 export function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
