72d2ae95ebb46bc823de13bdeb6223bdb7a17143
nunonam <nunonam@nunonamui-MacBookPro.local>
2020-10-29 09:26:22 +0900

    update

--- src/Switch/index.js
+++ src/Switch/index.js
@@ -1,34 +1,14 @@
 import React from 'react';
-import Toggle from 'react-native-toggle-element';
+import {Switch} from 'react-native';
 import { Nuno } from '../..';
 
 export default ({checked, disabled, onPress}) => {
   return (
-    // <Switch
-    //   value={checked}
-    //   onChange={onPress}
-    //   trackColor={{true: Nuno.config.themeColor, false: 'lightgray'}}
-    //   disabled={disabled}
-    // />
-    <Toggle
-      disabled
+    <Switch
       value={checked}
-      onToggle={onPress}
-      leftTitle="Left"
-      rightTitle="Right"
-      thumbButton={{
-        width: 30,
-        height: 30,
-        radius: 15,
-      }}
-      trackBar={{
-        activeBackgroundColor: Nuno.config.themeColor,
-        inActiveBackgroundColor: 'lightgray',
-        // borderActiveColor: '#86c3d7',
-        // borderInActiveColor: '#1c1c1c',
-        // borderWidth: 5,
-        width: 50,
-      }}
+      onChange={onPress}
+      trackColor={{true: Nuno.config.themeColor, false: 'lightgray'}}
+      disabled={disabled}
     />
   );
 }
