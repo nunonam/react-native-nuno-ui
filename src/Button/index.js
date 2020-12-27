f61df6c41d46af36232c8605c0c1da9ad97f7dbe
nuno <nunonam@gmail.com>
2020-12-27 14:21:09 +0900

    update

--- src/Button/index.js
+++ src/Button/index.js
@@ -1,122 +1,121 @@
 import React from 'react';
-import {TouchableOpacity, View, Modal} from 'react-native';
-import FastImage from 'react-native-fast-image';
-import { ImageViewer, Icon } from 'react-native-nuno-ui';
-import {isIphoneX, getStatusBarHeight} from 'react-native-iphone-x-helper';
+import {View, TouchableOpacity} from 'react-native';
+import Loader from '../Loader';
+import HView from '../HView';
+import Text from '../Text';
 import { color } from 'react-native-nuno-ui/style';
+import Seperator from 'react-native-nuno-ui/src/Seperator';
 
-export default function Image({
-  height,
-  width,
-  borderRadius,
-  borderColor,
-  borderWidth,
-  uri,
-  onPress,
-  resizeMode,
-  viewer,
-  viewerUris, // viewer가 true이면 viewerUris 에 배열로 꼭 넣어줘야 한다
-  viewerIndex, // viewer가 true이면 viewerIndex 에 꼭 넣어줘야 한다
-  style,
-}) {
-  const [imageViewer, setImageViewer] = React.useState(false);
-  if (!resizeMode) {
-    resizeMode = 'cover';
-  }
-  if (uri) {
-    if (onPress) {
-      return (
-        <TouchableOpacity onPress={onPress}>
-          <FastImage
-            style={{
-              height: height,
-              width: width,
-              borderRadius: borderRadius,
-              borderWidth: borderWidth,
-              borderColor: borderColor,
-              ...style,
-            }}
-            source={typeof(uri) === 'number' ? uri : {uri: uri}}
-            resizeMode={resizeMode}
-          />
-        </TouchableOpacity>
-      );
-    } else {
-      if (viewer) {
-        return (
-          <View>
-            <TouchableOpacity onPress={() => setImageViewer(true)}>
-              <FastImage
-                style={{
-                  height: height,
-                  width: width,
-                  borderRadius: borderRadius,
-                  borderWidth: borderWidth,
-                  borderColor: borderColor,
-                  ...style,
-                }}
-                source={typeof(uri) === 'number' ? uri : {uri: uri}}
-                resizeMode={resizeMode}
-              />
-            </TouchableOpacity>
-            <Modal
-              visible={imageViewer}
-              transparent={true}
-              hardwareAccelerated={true}
-              animationType={'slide'}
-              onRequestClose={() => {
-                setImageViewer(false);
-              }}
-            >
-              <ImageViewer
-                data={viewerUris}
-                index={viewerIndex}
-                enableSwipeDown={true}
-                onSwipeDown={() => setImageViewer(false)}
-              />
-              {/* <TouchableOpacity
-                onPress={() => setImageViewer(false)}
-                style={{
-                  position: 'absolute',
-                  top: 20 + (isIphoneX() ? getStatusBarHeight() : 0),
-                  right: 20,
-                }}>
-                <Icon name={'close'} size={20} color={color('white')} />
-              </TouchableOpacity> */}
-            </Modal>
-          </View>
-        );
-      } else {
-        return (
-          <View>
-            <FastImage
-              style={{
-                height: height,
-                width: width,
-                borderRadius: borderRadius,
-                borderWidth: borderWidth,
-                borderColor: borderColor,
-                ...style,
-              }}
-              source={typeof(uri) === 'number' ? uri : {uri: uri}}
-              resizeMode={resizeMode}
-            />
-          </View>
-        );
+export default function Button(props) {
+  let fontSize, fontWeight, paddingHorizontal, height;
+  const borderstyle = {borderWidth: 0.5, borderRadius: 4};
+  const textstyle = {color: 'white'};
+  switch (props.size) {
+    case 'small':
+      fontSize = 12;
+      fontWeight = 'normal';
+      paddingHorizontal = props.paddingHorizontal || 10;
+      height = 24;
+      if (props.rounded) {
+        borderstyle.borderRadius = 12;
       }
-
-    }
-  } else {
-    return (
-      <View
-        style={{
-          height: height,
-          width: width,
-          borderRadius: borderRadius,
-          borderWidth: borderWidth,
-          borderColor: borderColor,
-        }}
-      />
-    );
+      break;
+    case 'large':
+      fontSize = 16;
+      fontWeight = 'bold';
+      paddingHorizontal = props.paddingHorizontal || 20;
+      height = 44;
+      if (props.rounded) {
+        borderstyle.borderRadius = 22;
+      }
+      break;
+    case 'medium':
+    default:
+      fontSize = 13;
+      fontWeight = 'bold';
+      paddingHorizontal = props.paddingHorizontal || 10;
+      height = 34;
+      if (props.rounded) {
+        borderstyle.borderRadius = 17;
+      }
+      break;
   }
-}
+
+  // border style
+  if (props.borderColor) {
+    borderstyle.borderColor = props.borderColor;
+  } else {
+    if (props.color === color('white')) {
+      borderstyle.borderColor = color('gray');
+    } else {
+      borderstyle.borderWidth = 0;
+    }
+  }
+  if (props.borderRadius !== undefined) {
+    borderstyle.borderRadius = props.borderRadius;
+  }
+
+
+  // text color
+  if (props.disable) {
+    textstyle.color = color('gray');
+  } else {
+    if (props.textColor) {
+      textstyle.color = props.textColor;
+    } else {
+      if (props.color === color('white')) {
+        textstyle.color = color('darkgray');
+      }
+    }
+  }
+  return (
+    <HView>
+      <TouchableOpacity
+        style={{flex: props.stretch ? 1 : undefined}}
+        activeOpacity={props.loading ? 1 : 0.5}
+        onPress={props.onPress}
+        disabled={props.disable || props.loading}
+        >
+          <View style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: height,
+            paddingHorizontal: paddingHorizontal,
+            backgroundColor: props.disable ? color('lightgray') : props.color,
+            ...borderstyle,
+            }}>
+            {props.left}
+            <Seperator width={10} />
+            <Text
+              fontSize={fontSize}
+              fontWeight={fontWeight}
+              text={props.text}
+              style={textstyle}
+            />
+            <Seperator width={10} />
+            {props.right}
+          </View>
+          {props.loading && (
+            <View style={{
+              backgroundColor: props.color,
+              borderRadius: props.borderRadius !== undefined ? props.borderRadius : 4,
+              position: 'absolute',
+              top: 0,
+              bottom: 0,
+              left: 0,
+              right: 0,
+              alignItems: 'center',
+              justifyContent: 'center',
+              ...borderstyle,
+              }}>
+              <Loader color={props.color === color('white') ? color('black') : color('white')} />
+            </View>
+          )}
+          {props.bottom && (
+            <Seperator bottom color={props.disable ? color('lightgray') : props.color} />
+          )}
+      </TouchableOpacity>
+    </HView>
+  );
+};
